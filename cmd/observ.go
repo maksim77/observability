5d4c57c03ee9391f5c7a504fbe561f9d90ef304c
Сёмочкин Максим Викторович <mvsyomo1@mts.ru>
2023-10-25 10:44:42 +0300

    add router midleware

--- cmd/observ.go
+++ cmd/observ.go
@@ -5,17 +5,25 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"gitlab.services.mts.ru/teta/golang-for-university/observability/internal/logger"
 	"go.uber.org/zap"
+	"moul.io/chizap"
 )
 
 func main() {
 	r := chi.NewRouter()
 
-	logger, err := logger.GetLogger(true)
+	logger, err := logger.GetLogger(false)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	r.Use(middleware.RequestID)
+	r.Use(chizap.New(logger, &chizap.Opts{
+		WithReferer:   true,
+		WithUserAgent: true,
+	}))
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		_, err := w.Write([]byte("welcome"))
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.21.0
 require (
 	github.com/go-chi/chi v1.5.5
 	go.uber.org/zap v1.26.0
+	moul.io/chizap v1.0.3
 )
 
 require go.uber.org/multierr v1.10.0 // indirect
