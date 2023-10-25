f984ee1a82a96e8b2886e2bfc75b28942890cafc
Сёмочкин Максим Викторович <mvsyomo1@mts.ru>
2023-10-25 10:12:17 +0300

    add simple logger

--- cmd/observ.go
+++ cmd/observ.go
@@ -1,16 +1,29 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi"
+	"gitlab.services.mts.ru/teta/golang-for-university/observability/internal/logger"
+	"go.uber.org/zap"
 )
 
 func main() {
 	r := chi.NewRouter()
 
+	logger, err := logger.GetLogger(true)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome")) //nolint:errcheck
+		_, err := w.Write([]byte("welcome"))
+		if err != nil {
+			logger.Error("Error writing response", zap.Error(err))
+		}
 	})
+
+	logger.Info("Server started")
 	http.ListenAndServe(":8080", r) //nolint:errcheck
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module gitlab.services.mts.ru/teta/golang-for-university/observability
 
 go 1.21.0
 
-require github.com/go-chi/chi v1.5.5
+require (
+	github.com/go-chi/chi v1.5.5
+	go.uber.org/zap v1.26.0
+)
+
+require go.uber.org/multierr v1.10.0 // indirect
