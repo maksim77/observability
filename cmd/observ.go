9037e6c77978d84f5e7077403d8110b4e3b80fad
Сёмочкин Максим Викторович <mvsyomo1@mts.ru>
2023-10-25 10:57:46 +0300

    add logger from context

--- cmd/observ.go
+++ cmd/observ.go
@@ -1,23 +1,34 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/juju/zaputil/zapctx"
 	"gitlab.services.mts.ru/teta/golang-for-university/observability/internal/logger"
 	"go.uber.org/zap"
 	"moul.io/chizap"
 )
 
+func someFunc(ctx context.Context) {
+	logger := zapctx.Logger(ctx)
+	time.Sleep(1 * time.Second)
+	logger.Info("Hi from SomeFunc")
+}
+
 func main() {
 	r := chi.NewRouter()
 
-	logger, err := logger.GetLogger(false)
+	logger, err := logger.GetLogger(true)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	logger = logger.With(zap.Any("someKey", "someValue"))
 
 	r.Use(middleware.RequestID)
 	r.Use(chizap.New(logger, &chizap.Opts{
@@ -26,6 +37,8 @@ func main() {
 	}))
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		ctx := zapctx.WithLogger(r.Context(), logger)
+		someFunc(ctx)
 		_, err := w.Write([]byte("welcome"))
 		if err != nil {
 			logger.Error("Error writing response", zap.Error(err))
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.21.0
 
 require (
 	github.com/go-chi/chi v1.5.5
+	github.com/juju/zaputil v0.0.0-20190326175239-ef53049637ac
 	go.uber.org/zap v1.26.0
 	moul.io/chizap v1.0.3
 )
